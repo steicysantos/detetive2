cdd32d18966130445fe9419a03a8f2cf57734b76
Steicy <85196547+steicysantos@users.noreply.github.com>
2022-05-31 00:10:40 -0300

    tentano

---
+++ Detetive/Palpite.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace palpite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.cbAssassino = new System.Windows.Forms.ComboBox();
+            this.cbArma = new System.Windows.Forms.ComboBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.cbLocal = new System.Windows.Forms.ComboBox();
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbAssassino
+            // 
+            this.cbAssassino.FormattingEnabled = true;
+            this.cbAssassino.Location = new System.Drawing.Point(77, 240);
+            this.cbAssassino.Name = "cbAssassino";
+            this.cbAssassino.Size = new System.Drawing.Size(154, 21);
+            this.cbAssassino.TabIndex = 2;
+            // 
+            // cbArma
+            // 
+            this.cbArma.FormattingEnabled = true;
+            this.cbArma.Location = new System.Drawing.Point(323, 240);
+            this.cbArma.Name = "cbArma";
+            this.cbArma.Size = new System.Drawing.Size(154, 21);
+            this.cbArma.TabIndex = 3;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::palpite.Properties.Resources.aaaaa;
+            this.pictureBox4.Location = new System.Drawing.Point(323, 101);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(154, 111);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::palpite.Properties.Resources.Person2;
+            this.pictureBox3.Location = new System.Drawing.Point(77, 101);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(154, 111);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::palpite.Properties.Resources.locallll;
+            this.pictureBox2.Location = new System.Drawing.Point(560, 101);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(154, 111);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::palpite.Properties.Resources.fundo2;
+            this.pictureBox1.Location = new System.Drawing.Point(1, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(803, 453);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConfirmar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfirmar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnConfirmar.Location = new System.Drawing.Point(311, 320);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(183, 39);
+            this.btnConfirmar.TabIndex = 8;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            // 
+            // cbLocal
+            // 
+            this.cbLocal.FormattingEnabled = true;
+            this.cbLocal.Location = new System.Drawing.Point(560, 240);
+            this.cbLocal.Name = "cbLocal";
+            this.cbLocal.Size = new System.Drawing.Size(154, 21);
+            this.cbLocal.TabIndex = 9;
+            // 
+            // cb
+            // 
+            this.cb.FormattingEnabled = true;
+            this.cb.Location = new System.Drawing.Point(551, 195);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(154, 21);
+            this.cb.TabIndex = 9;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(285, 323);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(183, 39);
+            this.btn.TabIndex = 8;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbLocal);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.cbArma);
+            this.Controls.Add(this.cbAssassino);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbAssassino;
+        private System.Windows.Forms.ComboBox cbArma;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.ComboBox cbLocal;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ Detetive/Palpite.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace palpite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+
+        }
+
+        private void InitializeComboBox()
+        {
+            string[] enumElements1 = Enum.GetNames(typeof(Assassinos));
+            string[] enumElements2 = Enum.GetNames(typeof(Locais));
+            string[] enumElements3 = Enum.GetNames(typeof(Armas));
+
+            foreach(var item in enumElements1)
+            {
+                cbAssassino.Items.Add(item.Replace("_", " "));
+            }
+            foreach (var item in enumElements2)
+            {
+                cbLocal.Items.Add(item.Replace("_", " "));
+            }
+            foreach (var item in enumElements3)
+            {
+                cbArma.Items.Add(item.Replace("_", " "));
+            }
+        }
+    }
+}
